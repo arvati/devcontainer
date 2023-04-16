7a5a01b08166b8194227ed2d16140489b338fc78
Ademar Arvati Filho <arvati@hotmail.com>
2023-04-16 20:46:27 -0300

    force bash alpine

--- Node/alpine/library-scripts/common-alpine.sh
+++ Node/alpine/library-scripts/common-alpine.sh
@@ -16,7 +16,6 @@ USERNAME=${2:-"automatic"}
 USER_UID=${3:-"automatic"}
 USER_GID=${4:-"automatic"}
 INSTALL_OH_MYS=${5:-"true"}
-SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 MARKER_FILE="/usr/local/etc/vscode-dev-containers/common"
 
 # Switch to bash right away
@@ -26,6 +25,8 @@ if [ "${SWITCHED_TO_BASH}" != "true" ]; then
     exec /bin/bash "$0" "$@"
     exit $?
 fi
+
+SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 
 if [ "$(id -u)" -ne 0 ]; then
     echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
