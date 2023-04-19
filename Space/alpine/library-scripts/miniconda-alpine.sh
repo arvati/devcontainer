cbea29f306b408ffa27be79700e7c0f2bb9d1fc3
Ademar Arvati Filho <arvati@hotmail.com>
2023-04-19 18:46:39 -0300

    conda update

--- Space/alpine/library-scripts/miniconda-alpine.sh
+++ Space/alpine/library-scripts/miniconda-alpine.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-CONDA_VERSION=${1:-"py39_4.12.0"}
-CONDA_SHA256=${2:-"78f39f9bae971ec1ae7969f0516017f2413f17796670f7040725dd83fcff5689"}
+CONDA_VERSION=${1:-"py39_23.1.0-1"}
+CONDA_SHA256=${2:-"5dc619babc1d19d6688617966251a38d245cb93d69066ccde9a013e1ebb5bf18"}
 CONDA_DIR=${3:-"/opt/conda"}
 SWITCHED_TO_BASH=${4:-"true"}
 MARKER_FILE="/usr/local/etc/vscode-dev-containers/miniconda"
@@ -41,6 +41,7 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
     echo "**** get Miniconda ****"
     mkdir -p "$CONDA_DIR"
     wget "http://repo.continuum.io/miniconda/Miniconda3-${CONDA_VERSION}-Linux-x86_64.sh" -O miniconda.sh
+    https://repo.anaconda.com/miniconda/Miniconda3-py39_23.1.0-1-Linux-x86_64.sh
 
     
     
@@ -57,7 +58,7 @@ if [ "${PACKAGES_ALREADY_INSTALLED}" != "true" ]; then
         echo ". /etc/profile.d/base.sh" >> ~/.bashrc
 
         echo "**** setup Miniconda ****"
-        $CONDA_DIR/bin/conda init bash
+        $CONDA_DIR/bin/conda init --all
         $CONDA_DIR/bin/conda update --all --yes
         $CONDA_DIR/bin/conda config --set auto_update_conda False
 
